589d10adccb21bf0d7c0cb6375d949b23a28a27c
Javier Abadía <javi.abadia@gmail.com>
2017-10-06 15:00:14 +0200

    some comments

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from django.http import StreamingHttpResponse
 
 def generate_response():
     for i in range(10):
-        yield "hi! {}\n".format(i)
+        yield "hi! {}\n".format(i)  # yield strings to immediately send them to client
         time.sleep(1)
 
 
