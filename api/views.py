048062874e0d2dc8f8b7d02655e92a1a81e82a37
Javier Abadía <javi.abadia@gmail.com>
2017-10-06 16:16:29 +0200

    cancel fetch

--- api/views.py
+++ api/views.py
@@ -1,9 +1,14 @@
 import time
+import logging
+
 from django.http import StreamingHttpResponse
+
+logger = logging.getLogger(__name__)
 
 
 def generate_response():
     for i in range(10):
+        logger.warning('sending msg #{}'.format(i))
         yield "hi! {}\n".format(i)  # yield strings to immediately send them to client
         time.sleep(1)
 
